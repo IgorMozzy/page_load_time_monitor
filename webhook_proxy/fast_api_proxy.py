8fc55613f529f901709b72f2dfdc4f16faa9a922
sqfav <158740950+IgorMozzy@users.noreply.github.com>
2025-03-12 20:07:33 +0100

    fix

--- webhook_proxy/fast_api_proxy.py
+++ webhook_proxy/fast_api_proxy.py
@@ -1,13 +1,14 @@
 import os
+import requests
 from dotenv import load_dotenv
 from fastapi import FastAPI, Request
-import httpx
 
 app = FastAPI()
 
 load_dotenv()
 
 TARGET_URL = os.getenv("HOOK_ADDRESS")
+
 
 @app.post("/webhook")
 async def handle_webhook(request: Request):
@@ -16,10 +17,12 @@ async def handle_webhook(request: Request):
     transformed_data = {
         "text": payload.get('message'),
     }
-    async with httpx.AsyncClient(follow_redirects=True) as client:
-        try:
-            response = await client.post(TARGET_URL, json=transformed_data)
-        except:
-            print('error: problem_with_sending to:', TARGET_URL)
-        finally:
-            return {"status": "ok", "target_response": response.status_code}
+
+    try:
+        response = requests.post(TARGET_URL, json=transformed_data)
+        status_code = response.status_code
+    except requests.RequestException:
+        print('error: problem_with_sending to:', TARGET_URL)
+        status_code = None
+
+    return {"status": "ok", "target_response": status_code}
